c90541eb8688e5458b5e02abbed45fec8df8743b
liuhua <jyfc7879@gmail.com>
2021-01-01 22:33:36 +0800

    ..

--- src/headfirst/designpatterns/decorator/io/InputTest.java
+++ src/headfirst/designpatterns/decorator/io/InputTest.java
@@ -5,18 +5,30 @@ import java.io.*;
 public class InputTest {
 	public static void main(String[] args) throws IOException {
 		int c;
-
+		InputStream in = null;
 		try {
-			InputStream in = 
-				new LowerCaseInputStream(
+			in = 
+				new LowerCaseInputStream( 
 					new BufferedInputStream(
 						new FileInputStream("test.txt")));
 
 			while((c = in.read()) >= 0) {
 				System.out.print((char)c);
 			}
-
-			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (in != null) { in.close(); }
+		}
+		System.out.println();
+		try (InputStream in2 = 
+				new LowerCaseInputStream(
+					new BufferedInputStream(
+						new FileInputStream("test.txt")))) 
+		{
+			while((c = in2.read()) >= 0) {
+				System.out.print((char)c);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
